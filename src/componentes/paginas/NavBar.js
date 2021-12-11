cdf327477cd6d5db5fb9e45c26a146937e43a0bd
elton971 <82831720+elton971@users.noreply.github.com>
2021-12-11 13:17:01 -0300

    inicio 1

---
+++ src/componentes/paginas/NavBar.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function NavBar() {
+    return (
+        <>
+            <div className="navbar">
+                <div className="navbar-container container">
+                    <Link  to='/' className="navbar-logo">
+                        ELTON-WEB
+                    </Link>
+                    <div className="menu-icon">
+                        
+                    </div>
+                </div>
+
+            </div>
+        </>
+    )
+}
+
+export default NavBar
